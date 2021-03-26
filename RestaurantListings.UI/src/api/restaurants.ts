b9cb22b390ce7babce44127c65eea7a96426a9d6
Aadil Cachalia <aadil@codeassembly.co.za>
2021-03-27 01:34:11 +0200

    Feature: Save user rating
     -  Implement naive api endpoint placeholder

--- RestaurantListings.UI/src/api/restaurants.ts
+++ RestaurantListings.UI/src/api/restaurants.ts
@@ -4,3 +4,18 @@ export async function getRestaurants(): Promise<Restaurant[]> {
   const res = await fetch("/api/restaurants");
   return await res.json();
 }
+
+// Naive and simplistic implementation would use tokens or cookies to get the user session to save the rating 
+// no need to pass in the userId endpoint would need to ensure user is also logged in
+
+export async function saveUserRating(userRatingPayload: {
+  userId: any;
+  restaurantId: any;
+  rating: number;
+}): Promise<Restaurant[]> {
+  const res = await fetch("/api/restaurant/setUserRating", {
+    method: "POST",
+    body: JSON.stringify(userRatingPayload),
+  });
+  return await res.json();
+}
