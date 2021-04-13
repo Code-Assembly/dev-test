ef28880ed72e3a43d79f08728c54fd48f05251f7
Aadil Cachalia <aadil@codeassembly.co.za>
2021-04-13 12:50:19 +0200

    Feature: Implement the saveUserRating API on the UI

--- RestaurantListings.UI/src/api/restaurants.ts
+++ RestaurantListings.UI/src/api/restaurants.ts
@@ -1,21 +1,38 @@
 import { Restaurant } from "../interfaces/restaurant";
 
+import { authService } from "../auth/authService";
+
 export async function getRestaurants(): Promise<Restaurant[]> {
-  const res = await fetch("/api/restaurants");
+  const res = await fetch("/api/Restaurants");
   return await res.json();
 }
 
-// Naive and simplistic implementation would use tokens or cookies to get the user session to save the rating 
-// no need to pass in the userId endpoint would need to ensure user is also logged in
+export async function saveUserRating(restaurantId: number, userRating: number) {
+  let response: Response;
 
-export async function saveUserRating(userRatingPayload: {
-  userId: any;
-  restaurantId: any;
-  rating: number;
-}): Promise<Restaurant[]> {
-  const res = await fetch("/api/restaurant/setUserRating", {
+  const clientToken = await authService.getAccessToken();
+
+  if (!clientToken) {
+    console.log("Redirect to login page");
+  }
+
+  response = await fetch(`/api/Restaurants/${restaurantId}/RateRestaurant`, {
     method: "POST",
-    body: JSON.stringify(userRatingPayload),
+    credentials: "include",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+      Authorization: "Bearer " + clientToken,
+    },
+    body: JSON.stringify({ userRating }),
   });
-  return await res.json();
+
+  if (response.ok) {
+    console.log(response);
+    return (await response.json()) as Restaurant;
+  } else if (response.status === 401) {
+    console.log("Redirect to login page");
+  }
+
+  throw response;
 }
